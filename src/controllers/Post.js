89627501a4f44e9b6e4edb66ef7c13c7a3ca2ae5
Yago Amaral <yago.m.amaral@gmail.com>
2023-08-18 17:59:46 -0300

    added some Post controllers

--- src/controllers/Post.js
+++ src/controllers/Post.js
@@ -1,6 +1,24 @@
-export function getPosts(req, res) {
-    res.json({
-        title: "Blog Post",
-        content: "Lorem"
-    });
+import Post from "../models/Post.js";
+
+export async function getPosts(req, res) {
+    const posts = await Post.find({});
+
+    res.json(posts);
 }
+
+export async function getSinglePost(req, res) {
+    const { id } = req.params;
+    
+    try {
+        const post = await Post.findById(id);
+        res.json(post);
+    } catch (err) {
+        res.status(404).json({
+            error: "Recurso não encontrado",
+            message: err.message
+        });
+    }
+
+}
+
+export async function createPost(req, res) {}
